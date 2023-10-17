3e529cb1f83396fdeb136dfd76c3b9e70dd5cb74
Amon <142914143+am0npuppet@users.noreply.github.com>
2023-10-17 12:07:08 -0300

    Adicionando arquivo via upload.
    Resolução do exercício em Java em vetor.

---
+++ Lista - Exercícios Vetor/Quadrado.java
@@ -0,0 +1,32 @@
+package vetorex;
+import java.util.Scanner;
+
+public class Vetor {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int a[], b[], i;
+		final int TAM = 10;
+		
+		a = new int[TAM];
+		b = new int[TAM];
+		
+		for (i = 0; i < TAM; i++) {
+			System.out.println("Diite o " + (i + 1) + "º valor: ");
+			a[i] = in.nextInt();
+		}
+		
+		System.out.print("\nA = [ ");
+		for (i = 0; i < TAM; i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.print(" ]");
+		
+		System.out.print("\nB = [ ");
+		for (i = 0; i < TAM; i++) {
+			b[i] = a[i] * a[i];
+			System.out.print(b[i] + " ");
+		}
+		System.out.print(" ]");
+	}
+}
