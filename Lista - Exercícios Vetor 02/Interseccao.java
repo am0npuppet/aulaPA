95c4a5ab0c3c04819f88d38727d381a92103cbe1
Amon <142914143+am0npuppet@users.noreply.github.com>
2023-11-21 00:48:35 -0300

    Update Interseccao.java

--- Lista - Exercícios Vetor 02/Interseccao.java
+++ Lista - Exercícios Vetor 02/Interseccao.java
@@ -1,46 +1,46 @@
 import java.util.Scanner;
 
 public class Interseccao {
-	public static void main(String[] args) {
-	    
-		Scanner in = new Scanner(System.in);
-	    
-		int[] a, b, c;
-	    int i;
-		final int TAM = 10;
-		
-		a = new int [TAM];
-		b = new int [TAM];
-		c = new int [TAM];
-		
-		for (i = 0; i < TAM; i++) {
-			System.out.println("\nDigite o " + (i + 1) + "º número do vetor A: ");
-			a[i] = in.nextInt();
-		}
-		
-		for (i = 0; i < TAM; i++){
-			System.out.println("\nDigite o " + (i + 1) + "º número do vetor B: ");
-			b[i] = in.nextInt();
-		    
-			for (int g = 0; g < TAM; g++){
-			    
-				if (b[i] == a[g]) {
-					c[i] = a[g];
-				}
-			}
-		}
-	    
-		System.out.print("Os valores deo vetor C são: ");
-		for (i = 0; i < TAM; i++) {
-		    
-			if (c[i] == 0) {
-				
-			} else {
-				System.out.print(c[i] + " ");
-			}
-		}
-		
-	    in.close();
-	    System.exit(0);
-	}
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int[] a, b, c;
+        int i;
+        final int TAM = 10;
+
+        a = new int[TAM];
+        b = new int[TAM];
+        c = new int[TAM];
+
+        for (i = 0; i < TAM; i++) {
+            System.out.println("\nDigite o " + (i + 1) + "º número do vetor A: ");
+            a[i] = in.nextInt();
+        }
+
+        for (i = 0; i < TAM; i++) {
+            System.out.println("\nDigite o " + (i + 1) + "º número do vetor B: ");
+            b[i] = in.nextInt();
+
+            for (int g = 0; g < TAM; g++) {
+
+                if (b[i] == a[g]) {
+                    c[i] = a[g];
+                }
+            }
+        }
+
+        System.out.print("Os valores do vetor C são: ");
+        for (i = 0; i < TAM; i++) {
+
+            if (c[i] == 0) {
+
+            } else {
+                System.out.print(c[i] + " ");
+            }
+        }
+
+        in.close();
+        System.exit(0);
+    }
 }
